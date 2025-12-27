e09e8babcf6bdd16d84c3057f39444e248add929
unknown <shivrajdakore6@gmail.com>
2025-12-27 13:14:38 +0530

    Upload folder as static files

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 const express = require("express");
+const path = require("path");
 const app = express();
 
 const swaggerRouter = require("./swagger");
 const usersRouter = require("./routes/users");
 
 app.use(express.json());
+
+app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
 // Routes
 app.use("/api-docs", swaggerRouter);
